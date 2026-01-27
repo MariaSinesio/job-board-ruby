6a3da24f378e58c9cbc75a3bdbaf8c6afeea3ae4
MariaSinesio <marialuizavicentesinesio@gmail.com>
2026-01-27 17:49:05 -0300

    Feat: Criação das rotas

--- db/migrate/20260122114324_create_jobs.rb
+++ db/migrate/20260122114324_create_jobs.rb
@@ -4,7 +4,6 @@ class CreateJobs < ActiveRecord::Migration[8.0]
       t.string :description
       t.string :name
       t.references :category, null: false, foreign_key: true
-
       t.timestamps
     end
   end
