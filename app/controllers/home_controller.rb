8f6acd4bcf66e860a692f1587e8c43df450c0665
MariaSinesio <marialuizavicentesinesio@gmail.com>
2026-02-04 11:57:02 -0300

    Adicionando o html puro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @add_recently = Job.includes(:category).order(created_at: :desc).limit(6)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@
 #   end
 # Dados falsos injetados sem que tenha que digitar no form (Front)
 
-Category.create(label: "Jovem Aprendiz")
-Job.create(description: "A description", name: "Jovem aprendiz", category_id: 2)
+Job.create(description: "A description", name: "Product Designer", category_id: 4)
+Job.create(description: "A description", name: "Designer", category_id: 4)
