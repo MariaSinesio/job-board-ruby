392910085227bdf7fb9846ef77e77eda07e46c0f
Mia <142182687+MariaSinesio@users.noreply.github.com>
2026-03-13 09:16:05 -0300

    Feature front (#19)
    * Ci: Testando
    
    * Adicionando o html puro
    
    * Feat: Editando o header e o footer
    
    * style: ajustando footer e o header
    
    * Style: Ajustando o header e a section main
    
    * Style: Adicionando os cards e editando o espaçamento
    
    * Feat: Adicionando uma tabela e colunas
    
    * Fix: Corrigindo erro no ci
    
    * Feat: Adicionando e editando tabelas do pg
    
    * chore: atualizando gems
    
    * Feat: Adicionando acesso ao endpoint de detalhes de vaga
    
    * Feat: Adicionando acesso ao endpoint de detalhes de vaga
    
    * Chore: Atualizando schema em db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_27_132446) do
+
+ActiveRecord::Schema[8.0].define(version: 2026_03_04_141024) do
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,7 +30,17 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_27_132446) do
     t.datetime "updated_at", null: false
     t.decimal "salary_max"
     t.decimal "salary_min"
+    t.bigint "locales_id"
     t.index ["category_id"], name: "index_jobs_on_category_id"
+    t.index ["locales_id"], name: "index_jobs_on_locales_id"
+  end
+
+  create_table "locales", force: :cascade do |t|
+    t.string "locale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "state"
+    t.string "city"
   end
 
   create_table "locales", force: :cascade do |t|
@@ -41,4 +53,5 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_27_132446) do
   end
 
   add_foreign_key "jobs", "categories"
+  add_foreign_key "jobs", "locales", column: "locales_id"
 end
