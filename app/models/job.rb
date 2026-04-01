c57b21554331cee4b1ffedeef5cc8e767b29d894
Mia <142182687+MariaSinesio@users.noreply.github.com>
2026-04-01 11:05:54 -0300

    Feature Front - Adicionando features e ajustes na tela de login e home (#25)
    * Ci: Testando
    
    * Adicionando o html puro
    
    * Feat: Editando o header e o footer
    
    * style: ajustando footer e o header
    
    * Style: Ajustando o header e a section main
    
    * Style: Adicionando os cards e editando o espaçamento
    
    * Feat: Adicionando uma tabela e colunas
    
    * Fix: Corrigindo erro no ci
    
    * Feat: Adicionando e editando tabelas do pg
    
    * chore: atualizando gems
    
    * Feat: Adicionando acesso ao endpoint de detalhes de vaga
    
    * Feat: Adicionando acesso ao endpoint de detalhes de vaga
    
    * Feat: Adicionando acesso ao endpoint de detalhes de vaga
    
    * Style: Ajustando classes
    
    * Style: Ajustando a tela inicial e na tela do login
    
    * Chore: Atualizando dependências
    
    * Fix: Gemfile.lock causando divergência
    
    * Fix: Gemfile.lock causando divergência

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,15 @@ class Job < ApplicationRecord
   belongs_to :locale
   validates :name, presence: true
   validates :description, presence: true
+
+  def self.search(search)
+    if search
+    search_job = Job.find_by(search: job)
+    if search_job
+      self.where(job_id: search_job)
+    else
+      @jobs = Job.all
+    end
+    end
+  end
 end
--- db/migrate/20260227132446_add_details_to_jobs.rb
+++ db/migrate/20260227132446_add_details_to_jobs.rb
@@ -1,6 +1,6 @@
 class AddDetailsToJobs < ActiveRecord::Migration[8.0]
   def change
-    add_column :jobs, :salary_max, :decimal
-    add_column :jobs, :salary_min, :decimal
+    add_column :jobs, :salary_max, :decimal, precision: 24
+    add_column :jobs, :salary_min, :decimal, precision: 24
   end
 end
